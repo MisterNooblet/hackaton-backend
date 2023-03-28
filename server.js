c93d83f5f538f8456fc46bede8d293376338e440
mika <mikagivner123@gmail.com>
2023-03-28 23:09:38 +0300

    puppetteer

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import run from "./utils/puppeteer.js";
 import connectDB from "./config/connectDB.js";
- dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: "./config/config.env" });
 //middlewares
-const app = express()
-app.use(express.json())
-app.use(cors())
-connectDB()
+const app = express();
+app.use(express.json());
+app.use(cors());
+connectDB();
 
 if (process.env.NODE_ENV !== `production`) {
-    app.use(morgan(`dev`));
-  }
+  app.use(morgan(`dev`));
+}
 
-  app.get("/", (req, res) =>
+app.get("/", (req, res) =>
   res.status(200).json({ message: "server is running" })
 );
 
@@ -22,14 +23,12 @@ if (process.env.NODE_ENV !== `production`) {
 // app.use('/api/v1/account', AccountRoute)
 // app.use('/api/v1/transactions', TransactionsRoute)
 
-
 //error handlers
 
-
-
 //Server Listen
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
+run();
