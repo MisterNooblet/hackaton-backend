74bf28802ba76133882e93d3e073917b20010594
misternooblet <dragonroadsupport@gmail.com>
2023-03-29 00:18:00 +0300

    puppeteer done

--- utils/puppeteer.js
+++ utils/puppeteer.js
@@ -1,10 +1,13 @@
 import puppeteer from "puppeteer";
-const run = async () => {
+
+const scrapeData = async () => {
+
   let objCounter = 0;
   let infoArr = [];
   let dishArr = [];
   let finalArr = [];
   let countryName;
+
   const browser = await puppeteer.launch();
 
   //Tips Page
@@ -12,28 +15,28 @@ const run = async () => {
   await TipsPage.goto(
     "https://worldtravelers.org/travel-tips-tipping-guide.asp"
   );
-  const TipsHtml = await TipsPage.content();
-  const TipsText = await TipsPage.evaluate(() => document.body.innerText);
+
   const TipsCountriesArray = await TipsPage.evaluate(() =>
     Array.from(
       document.querySelectorAll(".bcolorcccccc>tbody>tr>td>span"),
       (e) => e.innerText
     )
   );
+
   for (let info of TipsCountriesArray) {
     if (objCounter % 4 === 0 && objCounter !== 0) {
       infoArr = [
         ...infoArr,
-        { country: info, tip: TipsCountriesArray[objCounter + 1] },
+        { name: info, tip: TipsCountriesArray[objCounter + 1].split('%').join('').split('-')[0].replace(/[^0-9]/g, '').slice(0, 10) },
       ];
     }
     objCounter++;
   }
+
   //Traditional Dish Page
   const DishPage = await browser.newPage();
+
   await DishPage.goto("https://en.wikipedia.org/wiki/National_dish#By_country");
-  const DishHtml = await DishPage.content();
-  const DishText = await DishPage.evaluate(() => document.body.innerText);
 
   const DishCountriesArray = await DishPage.evaluate(() =>
     Array.from(
@@ -46,14 +49,36 @@ const run = async () => {
 
   for (let dishTradition of DishCountriesArray) {
     countryName = dishTradition.split(":");
-    //let foods = countryName[1].split(",");
-    finalArr = [...finalArr, { country: countryName[0], food: countryName[1] }];
+
+
+    finalArr = [...finalArr, { name: countryName[0], cuisines: fixDishText(countryName[1]) }];
   }
 
+  const result = finalArr.reduce((acc, val) => {
+    let objWithTip = val
+    let countryWithTip = infoArr.find(({ name }) => name === val.name)
+    if (countryWithTip && countryWithTip.tip * 1 !== NaN && val.cuisines !== null) {
+      objWithTip = { ...val, tip: countryWithTip.tip * 1 }
+      acc.push(objWithTip)
+    } else if (val.cuisines !== null) {
+      objWithTip = { ...val, tip: 0 }
+      acc.push(objWithTip)
+    }
+    return acc
+  }, [])
+
   await browser.close();
-  console.log(`finalArr:`, finalArr);
-  console.log(`infoArr`, infoArr);
-  // console.log(`countryName:${countryName}`);
-  //console.log(DishCountriesArray[1]);
+
+
+  return result
 };
-export default run;
+
+const fixDishText = (string) => {
+  if (string) {
+    let result = string.split(/,\s*/).map(s => s.replace(/\[[^\]]*\]\s*/g, ''))
+    return result
+  }
+  return null
+}
+
+export default scrapeData;
